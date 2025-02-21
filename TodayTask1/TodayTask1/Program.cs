630ef5d05d4650b9f5899d6a742ee472818b822f
Donggun-Im <108616381+Donggun-Im@users.noreply.github.com>
2025-02-21 18:01:55 +0900

    study7

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //로딩바 시작화면
+            //게임스토리1
+
+            Console.WriteLine("로딩바 엔터를 치면 스토리가 시작됩니다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("한 남자가 정신을 차린다.");
+            Console.WriteLine("차가운 금속 바닥. 희미한 형광등 불빛이 깜빡인다.");
+            Console.WriteLine("그는 거대한 기계들이 줄지어 선 공장 내부에 홀로 서 있다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("그는 자신의 이름조차 기억나지 않는다.");
+            Console.WriteLine("하지만 직감적으로 느낌다. 여기서 나가야 한다");
+        }
+    }
+}
