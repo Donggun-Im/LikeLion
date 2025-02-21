3a92c77f32703f07b7a1987b427cd707821efa37
Donggun-Im <108616381+Donggun-Im@users.noreply.github.com>
2025-02-21 15:04:15 +0900

    Loading

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(3000); // 3000 3초다 (3000밀리초
+            Console.Clear();
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(3000); // 3000 3초다 (3000밀리초
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(3000); // 3000 3초다 (3000밀리초
+            Console.Clear();
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(3000); // 3000 3초다 (3000밀리초
+            Console.Clear();
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(3000); // 3000 3초다 (3000밀리초
+            Console.Clear();
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(3000); // 3000 3초다 (3000밀리초
+            Console.Clear();
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(3000); // 3000 3초다 (3000밀리초
+            Console.Clear();
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(3000); // 3000 3초다 (3000밀리초
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■□□"); 
+            Thread.Sleep(3000); // 3000 3초다 (3000밀리초
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(3000); // 3000 3초다 (3000밀리초
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■■");
+
+
+        }                                                                                                                                                                                                                                                                                          
+    }
+}
