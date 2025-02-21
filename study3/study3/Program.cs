477c03c0f0f2747082518c4e14c822ec40df2bb4
Donggun-Im <108616381+Donggun-Im@users.noreply.github.com>
2025-02-21 13:06:22 +0900

    study4

---
+++ study3/study3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string character = "캐릭터";
+            int Hp = 100;
+            float Att = 56.7f;
+            string CharacterName = "??";
+            char Grade = 'S';
+
+            Console.WriteLine(character);
+            Console.WriteLine("hp : " + Hp);
+            Console.WriteLine("att : " + Att);
+            Console.WriteLine("캐릭터이름 : " + CharacterName);
+            Console.WriteLine("등급 : " + Grade);
+        }
+    }
+}
---
+++ study4/study4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    string greeting;
+            //    greeting = "Hello, World!";
+
+            //    Console.WriteLine(greeting);
+            // ctrl + kc / ctrl + ku OR ctrl + /
+
+            //const double pi = 3.14159; //상수 pi 선언 및 초기화
+            //const int MaxScore = 100; //정수형 상수 MaxScore 선언 및 초기화
+
+            //Console.WriteLine("pi = {0}", pi);
+            //Console.WriteLine("Max Score = {0}", MaxScore);
+
+            //int attack = 16755;
+            //int maxHp = 78103;
+
+            //int stat1 = 36;
+            //int stat2 = 1017;
+            //int stat3 = 41;
+            //int stat4 = 611;
+            //int stat5 = 22;
+            //int stat6 = 39;
+
+            //Console.WriteLine("기본 특성");
+            //Console.WriteLine("공격력 : {0}", attack);
+            //Console.WriteLine("최대 생명력 : {0}", maxHp);
+            //Console.WriteLine("전투 특성");
+            //Console.WriteLine("치명 : {0}", stat1);
+            //Console.WriteLine("특화 : {0}", stat2);
+            //Console.WriteLine("제압 : {0}", stat3);
+            //Console.WriteLine("신속 : {0}", stat4);
+            //Console.WriteLine("인내 : {0}", stat5);
+            //Console.WriteLine("숙련 : {0}", stat6);
+
+
+        }
+    }
+}
